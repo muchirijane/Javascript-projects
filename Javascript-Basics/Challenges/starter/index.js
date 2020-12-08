c5c37ca20b64e3b26d67901e5eaec20c1ec73cc9
Jane Tracy Muthoni <muchirijane@outlook.com>
2020-12-08 19:11:36 +0300

    added challenge 4

--- Javascript-Basics/Challenges/starter/index.js
+++ Javascript-Basics/Challenges/starter/index.js
@@ -71,6 +71,8 @@ GOOD LUCK �
 
 */
 
+/*
+
 const markWeight = 78;
 const markHeight = 1.69; // 2.8224
 
@@ -94,4 +96,57 @@ if (markBMI > johnBMI) {
   console.log(`Mark\'s BMI is (${markBMI}) higher than John\'s (${johnBMI})!`);
 }else{
   console.log(`John\'s BMI is (${johnBMI}) higher than Mark\'s (${markBMI})!`);
+}
+
+*/
+
+/*
+Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+GOOD LUCK �
+*/
+
+// /****DATA 1 */
+
+// solution 1
+const dolphinsAvg = Math.floor((96 + 108 + 89) / 3);
+const koalasAvg = Math.floor((88 + 91 + 110) / 3);
+console.log (dolphinsAvg, koalasAvg);
+
+
+// //solution 2
+if(dolphinsAvg > koalasAvg){
+  console.log(`Dolphin wins with an avg score of ${dolphinsAvg}`);
+}else if(dolphinsAvg === koalasAvg){
+  console.log(`Dolphin's and Koalas team draw`);
+}else{
+  console.log(`Koalas team wins with an avg score of ${koalasAvg}`);
+}
+
+//solution Bonus 1 and bonus 2
+if(dolphinsAvg > koalasAvg && dolphinsAvg >= 100){
+  console.log(`Dolphin wins with an avg score of ${dolphinsAvg}`);
+}else if(dolphinsAvg === koalasAvg && dolphinsAvg >= 100 && koalasAvg >= 100){
+  console.log(`Dolphin's and Koalas team draw`);
+}else if(koalasAvg > dolphinsAvg && koalasAvg >= 100){
+  console.log(`Koalas team wins with an avg score of ${koalasAvg}`)
+}else{
+  console.log(`No team wins`);
 }
